684813d5d6a5b8e6574ffd33916e69bca78f8249
maxem <maxem@LAPTOP-J5OJLRDC.extern.kuleuven.be>
2021-03-01 22:57:15 +0100

    enkel implementatie nog voor mazemap !// gebruikt voor mijn comments

--- src/pacman/MazeMap.java
+++ src/pacman/MazeMap.java
@@ -6,8 +6,8 @@ import java.util.stream.IntStream;
 /**
  * Each instance of this class represents a maze layout, specifying the width and height of the maze
  * and, for each position in the maze, whether it is passable or not.
- * @invar 0 <= getWidth()
- * @invar 0 <= getHeight()
+ * @invar | 0 <= getWidth()
+ * @invar | 0 <= getHeight()
  */
 public class MazeMap {
 	
@@ -20,42 +20,52 @@ public class MazeMap {
 	 * Returns the width (i.e. the number of columns) of this maze map.
 	 * @basic	
 	 */
-	public int getWidth() { throw new RuntimeException("Not yet implemented"); }
+	public int getWidth() {
+		return width;
+	}
 	
 	/**
 	 * Returns the height (i.e. the number of rows) of this maze map.
 	 * @basic
 	 */
-	public int getHeight() { throw new RuntimeException("Not yet implemented"); }
+	public int getHeight() { 
+		return height;
+	}
 	
 	/**
 	 * Returns whether the square in this maze at row index {@code row} and column index {@code column} is passable.
 	 * The square in the top-left corner of the maze has row index 0 and column index 0.
-	 * @throws | rowIndex <= 0 || rowIndex >getWidth()
-	 * @throws | columnIndex <= 0 || columnIndex >getHeight()
+	 * @throws IndexOutOfBoundsException if the given rowIndex is less than zero or not less then the width of this MazeMap object
+	 * 		| rowIndex < 0 || rowIndex > getWidth() //! ik denk strict kleiner omdat (0,0) nog bij de map hoort?
+	 * @throws IndexOutOfBoundsException if the given columnIndex is less than zero or not less then the width of this MazeMap object
+	 *		| columnIndex < 0 || columnIndex > getHeight() 
 	 * @post   | result == true || result == false
 	 * 
 	 * 
 	 * 
 	 */
-	public boolean isPassable(int rowIndex, int columnIndex) { throw new RuntimeException("Not yet implemented"); }
+	public boolean isPassable(int rowIndex, int columnIndex) { 
+		if (rowIndex < 0 || rowIndex > getWidth()) throw new IndexOutOfBoundsException();
+		if (columnIndex < 0 || columnIndex > getHeight()) throw new IndexOutOfBoundsException();
+		
+	}
 	
 	/**
 	 * //Defensief te werk gaan voor ongeldige argumenten constructor 
 	 * Initializes this object so that it represents a maze layout with the given width, height, and
 	 * passable positions. The passable positions are given in row-major order (i.e. the first {@code width} elements
 	 * of {@code passable} specify the passability of the maze positions in the first row of the maze). 
-	 * @throws | width <= 0 //strict kleiner mss?
-	 * @throws | height <= 0 //strict kleiner mss?
-	 * @throws | passable == null 
-	 * @throws | passable.length == width*height
+	 * @throws IllegalArgumentException if the given width is smaller than one.
+	 * 		| width <= 0 //strict kleiner mss? !// denk het niet omdat dit echt het aantal is? kunnen het later nog aanpassen
+	 * @throws IllegalArgumentException if the given height is smaller than one.
+	 * 		| height <= 0 //strict kleiner mss?
+	 * @throws IllegalArgumentException if passable is null.
+	 * 		| passable == null 
+	 * @throws IllegalArgumentexception if the length of passable is not equal to the product of the given width and the given height.
+	 * 		| passable.length != width*height
 	 * 
 	 * @post   | getWidth() == width
 	 * @post   | getHeight() == height
-	 *
-	 * 
-	 * 
-	 * 
 	 *         
 	 *               
 	 * 
