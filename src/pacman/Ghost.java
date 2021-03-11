7d34e41484942d0997cd242f729f6d32e6c66922
abdirahman <abdirahman@DESKTOP-R6VS7KS.home>
2021-03-11 19:34:22 +0100

    Ghost

--- src/pacman/Ghost.java
+++ src/pacman/Ghost.java
@@ -1,18 +1,29 @@
 package pacman;
 
 import java.util.Random;
+import java.util.stream.IntStream;
 
 /**
  * Each instance of this class represents a ghost in a Pac-Man maze.
+ * 
+ * @invar 	| getSquare() != null
+ * @invar 	| getDirection() != null
  */
 public class Ghost {
-	
+	 
+	/**
+	 * @invar 	| square != null
+     * @invar 	| direction != null
+     * 
+     * @representationObject
+	 */
 	private Square square;
 	private Direction direction;
 	
 	/**
 	 * Returns the square of a ghost object.
 	 * 
+	 * @basic
 	 */
 	public Square getSquare() {
 		return square;
@@ -20,6 +31,8 @@ public class Ghost {
 	
 	/**
 	 * Returns the direction in which this ghost will preferably move next.
+	 * 
+	 * @basic
 	 */
 	public Direction getDirection() { 
 		return direction; 
@@ -29,10 +42,20 @@ public class Ghost {
 	 * Initializes this object so that it represents a ghost enemy who the player has to avoid. 
 	 * Each instance of this object has a given square, the position where it is, and a direction in which it is moving.
 	 * 
+	 * @throws IllegalArgumentException if the given square is null.
+	 *  		| square == null 
+	 * @throws IllegalArgumentException if the given direction is null.
+	 * 	  		| direction == null 
 	 * @post | getSquare() == square
 	 * @post | getDirection() == direction
 	 */
 	public Ghost(Square square, Direction direction) { 
+		if (square == null) {
+			throw new IllegalArgumentException("square is null");
+		}
+		if (direction == null) {
+			throw new IllegalArgumentException("direction is null");
+		}
 		this.square = square;
 		this.direction = direction;
 	}
@@ -40,16 +63,27 @@ public class Ghost {
 	/**
 	 * Changes the square of the ghost object to a given square.
 	 * 
+	 * @throws IllegalArgumentException if the given square is null.
+	 *  		| square == null 
+	 * @mutates | this
 	 */
 	public void setSquare(Square square) { 
+		if (square == null) {
+			throw new IllegalArgumentException("square is null");
+		}
 		this.square = square; 
 	}
 	
 	/**
 	 * Sets the direction of the ghost object.
 	 * 
+	 * @throws IllegalArgumentException if the given direction is null.
+	 * 		| direction == null 
 	 */
 	public void setDirection(Direction direction) { 
+		if (direction == null) {
+			throw new IllegalArgumentException("direction is null");
+		}
 		this.direction = direction; 
 	}
 	
