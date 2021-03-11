3b816d7feca6ca7b1f0277538047355251033333
abdirahman <abdirahman@DESKTOP-R6VS7KS.home>
2021-03-11 19:41:50 +0100

    DotTest

--- src/pacman/test/DotTest.java
+++ src/pacman/test/DotTest.java
@@ -3,12 +3,19 @@ package pacman.test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import pacman.Dot;
+import pacman.MazeMap;
+import pacman.Square;
 
 class DotTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		MazeMap mazemap = new MazeMap(2, 2, new boolean[] {true,true,true,false});
+		Square square = Square.of(mazemap, 0, 1);
+		Dot dot = new Dot(square);
+		assertEquals(square, dot.getSquare()); 
+ 
 	}
 
-}
+} 
