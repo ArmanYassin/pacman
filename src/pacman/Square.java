214db7125413fee2d0b8f9b27debafff48eac553
abdirahman <abdirahman@DESKTOP-R6VS7KS.home>
2021-03-11 19:37:45 +0100

    Square

--- src/pacman/Square.java
+++ src/pacman/Square.java
@@ -1,7 +1,7 @@
 package pacman;
 
 import java.util.Arrays;
-
+import java.util.stream.IntStream;
 
 import pacman.MazeMap;
 
@@ -15,7 +15,12 @@ import pacman.MazeMap;
  * @invar | getMazeMap() != null
  */
 public class Square {
-	
+	/**
+	 * @invar | 0 <= rowIndex
+     * @invar | 0 <= columnIndex
+     * @invar | mazeMap != null
+	 * @representationObject
+	 */
 	private final MazeMap mazeMap;
 	private final int rowIndex;
 	private final int columnIndex;
@@ -50,17 +55,18 @@ public class Square {
 	
 	/**
 	 * returns if the object Square is passable 
-	 * 
-	 * @post | getMazeMap().isPassable(getRowIndex(),getColumnIndex())
+	 *  @inspects | this 
+	 *  @post | result == getMazeMap().isPassable(getRowIndex(), getColumnIndex())
+	 *  
 	 */
 	
 	public boolean isPassable() { 
-		return mazeMap.isPassable(rowIndex, columnIndex);
-		
+		return this.getMazeMap().isPassable(rowIndex, columnIndex);	
 	}
 	
 	/**
 	 * Initialises this object to represent a square in a given location
+	 * Normaal geen formele documentatie nodig
 	 * @post | getMazeMap() == mazeMap
 	 * @post | getRowIndex() == rowIndex
 	 * @post | getColumnIndex() == columnIndex
@@ -81,6 +87,10 @@ public class Square {
 	 *		| columnIndex < 0 || columnIndex > mazeMap.getHeight()
 	 * @throws IllegalArgumentException if MazeMap is null.
 	 * 		| mazeMap == null 
+	 * @post | result != null
+	 * @post | result.getMazeMap() == mazeMap
+	 * @post | result.getRowIndex() == rowIndex
+	 * @post | result.getColumnIndex() == columnIndex
 	 * 
 	 */
 	public static Square of(MazeMap mazeMap, int rowIndex, int columnIndex) {
@@ -90,8 +100,8 @@ public class Square {
 			throw new IllegalArgumentException("mazeMap is null");
 		return new Square(mazeMap,rowIndex,columnIndex);
 	}
-	
-	/**
+	 
+	/** 
 	 * Returns this square's neighbor in the given direction.
 	 * If this square has no neigbor in the given direction, return the square that is furthest away in the opposite direction.
 	 */
@@ -99,21 +109,21 @@ public class Square {
 	public Square getNeighbor(Direction direction) {
 		// Implementation hint: use method java.lang.Math.floorMod.
 		Square current = this;
-		Square neighbor = current;
+		//Square neighbor = new Square();
 
 		if(direction == Direction.DOWN )
-			neighbor = of(this.mazeMap, Math.floorMod(this.rowIndex+1,this.mazeMap.getHeight()), this.columnIndex);
+			return of(this.mazeMap, Math.floorMod(this.rowIndex+1,this.mazeMap.getHeight()), this.columnIndex);
 		if(direction == Direction.UP)
-			neighbor = of(this.mazeMap, Math.floorMod(this.rowIndex-1,this.mazeMap.getHeight()), this.columnIndex);
+			return of(this.mazeMap, Math.floorMod(this.rowIndex-1,this.mazeMap.getHeight()), this.columnIndex);
 		if(direction == Direction.LEFT)
-			neighbor = of(this.mazeMap, this.rowIndex, Math.floorMod(this.columnIndex-1,this.mazeMap.getWidth()));
+			return of(this.mazeMap, this.rowIndex, Math.floorMod(this.columnIndex-1,this.mazeMap.getWidth()));
 		if(direction == Direction.RIGHT)
-			neighbor = of(this.mazeMap, this.rowIndex, Math.floorMod(this.columnIndex+1,this.mazeMap.getWidth()));
+			return of(this.mazeMap, this.rowIndex, Math.floorMod(this.columnIndex+1,this.mazeMap.getWidth()));
 		
-		return neighbor;
+		return null;
 		
 		
-		//throw new RuntimeException("Not yet implemented");
+
 	}
 
 	/**
@@ -122,8 +132,7 @@ public class Square {
 	// No formal documentation required
 	public boolean canMove(Direction direction) {
 		return this.getNeighbor(direction).isPassable();
-		
-		//throw new RuntimeException("Not yet implemented");
+
 	}
 
 	/**
@@ -144,6 +153,7 @@ public class Square {
             } 
             possibleDirection[k++] = allDirections[i]; 
         }
+		
 		int count = 0; Direction val;
 		for(int i = 0; i<possibleDirection.length; i++) {
 			if(canMove(possibleDirection[i])) {
@@ -168,20 +178,24 @@ public class Square {
 			
 			
 		return passableDirection;
-		//throw new RuntimeException("Not yet implemented");
+
 	}
 	
 	/**
 	 * Returns whether the given square refers to the same {@code MazeMap} object and has the same row and column index as this square.
-	 *@throws IllegalArgumentException if the given square is null.
-	 * 		| other == null 
-	 * @post | result == this.equals(other)
+	 * @inspects | this, other
+	 * @throws IllegalArgumentException if the given square is null.
+	 * 		 | other == null 
+	 * @post | result ==(this==other)
+	 * 
 	 */
+	
 	public boolean equals(Square other) {
 		if (other == null)
 			throw new IllegalArgumentException("Given square is null");
-		return this.equals(other);
-		//throw new RuntimeException("Not yet implemented");
+		return this == other;
+
 	}
 	
+
 }
