461d574e08be0ed28351713a3af51047c0d0292f
abdirahman <abdirahman@DESKTOP-R6VS7KS.home>
2021-03-11 19:33:16 +0100

    dot

--- src/pacman/Dot.java
+++ src/pacman/Dot.java
@@ -2,27 +2,38 @@ package pacman;
 
 /**
  * Each instance of this class represents a dot (a piece of food for Pac-Man) in a Pac-Man maze.
+ * @immutable
+ * @invar | getSquare() != null
  */
 public class Dot {
-	
+	/**
+	 * @invar | square != null
+	 * 
+	 * @representationObject
+	 */
 	private Square square;
 	
-	
+	 
 	/**
 	 * Returns the square of a dot object.
+	 *  @basic
 	 * 
 	 */
 	public Square getSquare() {
 		return square;
-	}
-	
+	} 
+	  
 	/**
 	 * 
 	 * Initializes this dot object with a given square.
+	 * @throws IllegalArgumentException if the given square is null.
+	 * 		 | square == null 
+	 * @post |  getSquare() == square
 	 */
-	
+	 
 	public Dot(Square square) { 
 		this.square = square;
 	}
+ 
 
 }
