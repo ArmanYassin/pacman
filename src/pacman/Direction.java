a2fd263f94f7b346683babe07398be99809e4ab0
maxem <maxem@LAPTOP-J5OJLRDC.extern.kuleuven.be>
2021-03-12 10:15:59 +0100

    ok

--- src/pacman/Direction.java
+++ src/pacman/Direction.java
@@ -16,40 +16,5 @@ public enum Direction {
 		};
 	}
 
-	//klein test 'suit' Mag genegeerd worden. 
-	public static void main(String args[]) 
-	{ 
-		Direction[] allDirections = new Direction []{Direction.LEFT, Direction.RIGHT,Direction.UP, Direction.DOWN};
-		Direction [] possibleDirection = new Direction [3];
-		int pos = 0;
-		for(int i = 0; i<4; i++)
-			if(allDirections[i]==RIGHT)
-				pos = i;
-		for (int i = 0, k = 0; i < allDirections.length; i++) { 
-			if (i == pos) { 
-            continue; 
-        } 
-        possibleDirection[k++] = allDirections[i]; 
-	    }
-		boolean [] canMove = new boolean [] {true, false, true};
-		int count = 0; 
-		for(int i = 0; i<possibleDirection.length; i++) {
-			if(canMove[i]) {
-				count++; 						
-			}	
-		}
-		Direction [] passableDirection = new Direction [count];
-		
-		for(int i = 0,k=0; i<possibleDirection.length; i++) {
-			if(!canMove[i]) {
-				continue;
-			}
-			passableDirection[k++] = possibleDirection[i];
-		}
-		
-		
-
-
-		System.out.println("test");
-	} 
+	
 }
